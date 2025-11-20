ac6bb6208398589b41a24af74af2761326caa80d
dev-abbas <muhammad.abbas@ccript.com>
2025-11-20 15:03:44 +0500

    build

--- src/app/_api/shared.ts
+++ src/app/_api/shared.ts
@@ -1,3 +1,6 @@
-export const GRAPHQL_API_URL = process.env.NEXT_BUILD
-  ? `http://127.0.0.1:${process.env.PORT || 5000}`
-  : process.env.NEXT_PUBLIC_SERVER_URL
+const DEFAULT_INTERNAL_URL = `http://127.0.0.1:${process.env.PORT || 5000}`
+
+const isServer = typeof window === 'undefined'
+
+export const GRAPHQL_API_URL =
+  process.env.NEXT_BUILD || isServer ? DEFAULT_INTERNAL_URL : process.env.NEXT_PUBLIC_SERVER_URL
